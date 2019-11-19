27fdc7f09986489c739787b3f4a665ed5a42b6f2
John Strand <john.strand@dekiru.se>
2019-11-19 21:40:29 +0100

    Now with useCallback

--- src/useasync.ts
+++ src/useasync.ts
@@ -1,9 +1,9 @@
-import { useReducer } from "react";
+import { useReducer, useCallback } from "react";
 
 export enum AsyncState {
-    Pending,
-    Done,
-    Error
+  Pending,
+  Done,
+  Error
 }
 
 type State<T> = { value: T; state: AsyncState };
@@ -11,34 +11,37 @@ type AsyncFunction<T extends any[], R> = (...args: T) => Promise<R>;
 type ReturnType<T extends any[], R> = [R, AsyncState, (...args: T) => void];
 
 export default function useAsync<T extends any[], R>(
-    method: AsyncFunction<T, R>
+  method: AsyncFunction<T, R>
 ): ReturnType<T, R | undefined>;
 export default function useAsync<T extends any[], R>(
-    method: AsyncFunction<T, R>,
-    initialValue: R
+  method: AsyncFunction<T, R>,
+  initialValue: R
 ): ReturnType<T, R>;
 export default function useAsync<T extends any[], R>(
-    method: AsyncFunction<T, R>,
-    initialValue?: R
+  method: AsyncFunction<T, R>,
+  initialValue?: R
 ): ReturnType<T, R> {
-    const [{ value, state }, dispatch] = useReducer(
-        (state: State<R>, action: Partial<State<R>>) => ({
-            ...state,
-            ...action
-        }),
-        { value: initialValue as R, state: AsyncState.Pending }
-    );
+  const [{ value, state }, dispatch] = useReducer(
+    (state: State<R>, action: Partial<State<R>>) => ({
+      ...state,
+      ...action
+    }),
+    { value: initialValue as R, state: AsyncState.Pending }
+  );
 
-    const invoke = (...args: T) => {
-        dispatch({ state: AsyncState.Pending });
-        method(...args)
-            .then(value => {
-                dispatch({ value, state: AsyncState.Done });
-            })
-            .catch(err => {
-                dispatch({ value: err, state: AsyncState.Error });
-            });
-    };
+  const invoke = useCallback(
+    (...args: T) => {
+      dispatch({ state: AsyncState.Pending });
+      method(...args)
+        .then(value => {
+          dispatch({ value, state: AsyncState.Done });
+        })
+        .catch(err => {
+          dispatch({ value: err, state: AsyncState.Error });
+        });
+    },
+    [method]
+  );
 
-    return [value, state, invoke];
+  return [value, state, invoke];
 }
